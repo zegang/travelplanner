a95600640f8d99201e4e96fae7127d5c370304f6
zegang <zegang.luo@qq.com>
2025-09-22 13:09:01 +0800

    Wechat Permission

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { viteSingleFile } from 'vite-plugin-singlefile';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +14,14 @@ export default defineConfig({
     vueDevTools(),
     viteSingleFile({
       removeViteModuleLoader: false,
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '3a33b21966c70f9fc251c59a07c0ae2d.txt',
+          dest: ''
+        }
+      ]
     }),
   ],
   resolve: {
